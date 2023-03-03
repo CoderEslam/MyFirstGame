b7be589b34403a7530c54fcd2374a9c32c08a0dc
CoderEslam <eslamghazy600@gmail.com>
2023-03-03 22:56:22 +0200

    My first game unity

---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from unity Start eslam ghazy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log("Hello from unity update");
+    }
+}
